79c1be07d1a72cf73e3ec89eee67ee3124e4955d
Your Name <you@example.com>
2018-10-08 23:10:01 +0300

    pasisveikinimas

--- src/lt/kaunascoding/Main.java
+++ src/lt/kaunascoding/Main.java
@@ -3,6 +3,7 @@ package lt.kaunascoding;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("Labas, iveskite skaiciu: ");
     }
 }
