8ce9e187f94510c0e9b3b4636c207dddf5b33d42
Your Name <you@example.com>
2018-10-09 18:12:01 +0300

    skaiciaus kvadrato skaiciavimas

--- src/lt/kaunascoding/Main.java
+++ src/lt/kaunascoding/Main.java
@@ -1,7 +1,8 @@
 package lt.kaunascoding;
 
 import java.util.Scanner;
-
+//sukurti nauja branc kuriame butu igyvendintas metodas ivesto skaiciaus kelimui kvadratu
+//panaudoti feature branch, sukurti pull request ir atlikti merge
 public class Main {
 
     public static void main(String[] args) {
@@ -9,6 +10,12 @@ public class Main {
         System.out.println("Labas, iveskite skaiciu: ");
         Scanner sc = new Scanner(System.in);
         int ivestasSkaicius = sc.nextInt();
-        System.out.println("Ivestas skaicius: " + ivestasSkaicius);
+
+        int rezultatas = (ivestasSkaicius * ivestasSkaicius);
+        System.out.println("Skaiciaus kvadratas: " + rezultatas);
+    }
+
+    public double kvadratas(int a) {
+        return Math.pow(2, a);
     }
 }
